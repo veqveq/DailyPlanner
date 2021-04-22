865ac8312c86c23769224883d063c9ea4c2f986d
vasyakov_ey <vasyakov94@gmail.com>
2021-04-22 13:46:49 +0300

    Integrate CSS in project and correct JavaFX integration classes

--- frontend/src/main/java/ru/veqveq/client/gui/ClientFxApplication.java
+++ frontend/src/main/java/ru/veqveq/client/gui/ClientFxApplication.java
@@ -1,24 +1,24 @@
 package ru.veqveq.client.gui;
 
-
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import net.rgielen.fxweaver.core.FxWeaver;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
+import ru.veqveq.client.ClientApplication;
 
 public class ClientFxApplication extends Application {
     private ConfigurableApplicationContext applicationContext;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/ru/veqveq/client/gui/main-scene.fxml"));
-        primaryStage.setTitle("Hello World");
+        FxWeaver fxWeaver = applicationContext.getBean(FxWeaver.class);
+        Parent root = fxWeaver.loadView(MainController.class);
         primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Hello World");
         primaryStage.show();
     }
 
@@ -26,7 +26,7 @@ public class ClientFxApplication extends Application {
     public void init() throws Exception {
         String [] args = getParameters().getRaw().toArray(new String[0]);
         this.applicationContext = new SpringApplicationBuilder()
-                .sources(SpringBootApplication.class)
+                .sources(ClientApplication.class)
                 .run(args);
     }
 
--- frontend/src/main/java/ru/veqveq/client/gui/MainController.java
+++ frontend/src/main/java/ru/veqveq/client/gui/MainController.java
@@ -1,10 +1,11 @@
 package ru.veqveq.client.gui;
 
+import lombok.RequiredArgsConstructor;
 import net.rgielen.fxweaver.core.FxmlView;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 @FxmlView("main-scene.fxml")
 public class MainController {
-
 }
